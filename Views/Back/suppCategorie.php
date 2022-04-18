25275862e739896f74b4a9b03099dfe8287bea3a
TristanMendes <tristanmendes.voufonguefack@esprit.tn>
2022-04-18 17:15:59 +0100

    commit

--- Views/Back/suppCategorie.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include 'C:/xampp/htdocs/Artistes/Controller/CategorieC.php';
-	$categorieC=new CategorieC();
-	$categorieC->deletecategorie($_GET["ID"]);
-	header('Location:AfficherCategorie.php');
-?>
