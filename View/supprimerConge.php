cc7c9918655063445a41cfdc2cdb011aff18026d
Omar <omar.elkar.oe@gmail.com>
2022-04-22 01:55:11 +0100

    ds

--- View/supprimerConge.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include '../Controller/crud_func.php';
-	$CongeC=new CongesC();
-	$CongeC->supprimerConge($_GET["id_conge"]);
-	header('Location:AFficher.php');
-?>
--- View/supprimertypeC.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include '../Controller/crud_func.php';
-	$typeC=new typeC();
-	$typeC->supprimertypeC($_GET["id_typeC"]);
-	header('Location:AFficher.php');
-?>
